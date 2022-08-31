23c139d227a57e850652c08c46ad66335b4c9564
Vladislav <sdvlad75@gmail.com>
2022-08-31 13:13:36 +0300

    Added styles to button in timer

--- client/src/components/Timer/Button.js
+++ client/src/components/Timer/Button.js
@@ -28,9 +28,7 @@ const Button = () => {
     };
 
     return (
-    <button style={isRunning
-                    ? {color: '#DC2626', backgroundColor: '#FEE2E2', border: '2px solid #DC2626'}
-                    : {color: '#0E7490', backgroundColor: '#CFFAFE', border: '2px solid #0E7490'}} className={classes.timer_button} onClick={toggleTimer}>{isRunning ? "STOP" : "START"}</button>
+    <button className={isRunning ? classes.timer_button_inactive : classes.timer_button_active} onClick={toggleTimer}>{isRunning ? "STOP" : "START"}</button>
   )
 }
 
